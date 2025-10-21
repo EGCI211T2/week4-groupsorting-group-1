efa398e3bb032ef117a6b1765bdee193345f33d4
Thanasanan <thanasanan.ler@student.mahidol.edu>
2025-10-21 03:53:10 +0000

    เสร็ข้อแรก มั้ง

--- sorting.h
+++ sorting.h
@@ -9,23 +9,6 @@ void display(int a[],int n){
     printf("\n");
   
 }
-
-void selectionSort(int data[], int length) 
-{ 
-	int i, j, m, mi; 
-	
-     display(data,length);
-     
-	
-} 
-
-
-
-void insertion(int a[],int n){
-
-
-}
-
 void swap(int *a,int *b){
  int temp;
   temp=*a;
@@ -34,14 +17,82 @@ void swap(int *a,int *b){
   
 }
 
+void selectionSort(int data[], int length) 
+{ 
+	int i, j, m, mi; 
+	
+     display(data,length);
+
+     for (i = 0; i < length - 1; i++){
+mi=i;
+for(j = i + 1; j < length; j++){
+if (data[j] < data[mi]) {
+
+
+
+  mi = j;
+}
+if (mi != i){
+  swap(&data[i], &data[mi]);
+}
+
+
+}
+
+
+     }
+     
+	
+} 
+
+
+
+void insertion(int a[],int n){
+int i, j, key;
+for (i = 1; i < n; i++){
+key = a[i];
+j = i - 1;
+while (j >= 0 && a[j] > key){
+
+  a[j + 1] = a[j];
+            j = j - 1;
+}
+a[j + 1] = key;
+display(a, n);
+}
+
+}
+
+
+
 void bubbleSort(int a[],int n){
 
 int i,j;
 int sorted;
 // how may pair to compare?
 for(j=1;j<=n-1;j++){
+
  
   display(a,n);
+sorted = 1;
+for (i = 0; i < n - j; i++){
+if (a[i] > a[i + 1]) {
+swap(&a[i], &a[i + 1]);
+sorted = 0;
+
+
+
+
+}
+
+
+
+
+
+}
+if (sorted){
+  break;
+}
  }
   
 }
